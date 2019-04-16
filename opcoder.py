f79f4e0536e98413dac37084b09677e390b769da
FindHao <find@findhao.net>
2019-04-16 21:16:54 +0800

    add readme; add output of mapping between opcode and bit positions

--- opcoder.py
+++ opcoder.py
@@ -2,39 +2,58 @@
 """This file is used to """
 import logging
 import os
-import sys
-
-import com_lib
 import re
 from dumper import dump
 from inst import Inst
+import com_lib
 
 sass_content = ''
 code_line_reg = re.compile(r'/\*.{4}\*/ +(.+?); +?/\* 0x(.+) \*/\n.+?/\* 0x(.{16}) \*/')
+# example:['MOV': [1,2,3,4,5], 'ADD': [1,2,3,4,5,6,7,8]]
+# record opcode and the bit position
+ops = {}
+
+
+def init_sass_cubin_files(input_file, arch):
+    """
+    disasm input cubin file to sass and copy it to data/$arch/
+    @:arg input_file: input cubin file
+    """
+    cmd = com_lib.DISASM_BIN_TO_PTX_CMD % (
+        com_lib.CUDA_TOOLKIT, arch, input_file, "%s.sass" % os.path.splitext(input_file)[0])
+    tmp_read = os.popen(cmd).read()
+    if tmp_read:
+        logging.error("Error: when disasm input cubin file:\t%s" % tmp_read)
+    cmd = "cp %s data/%s/%s.tmp.cubin" % (input_file, arch, arch)
+    tmp_read = os.popen(cmd).read()
+    if tmp_read:
+        logging.error("Error: when copy input cubin file:\t%s" % tmp_read)
 
 
 def work():
+    global sass_content, ops
+
+    arch = "sm_75"
+    # init_sass_cubin_files('test.cubin', arch)
     logging.basicConfig(level=logging.INFO)
-    global sass_content
     with open("test.sass", 'r')as fin:
         sass_content = fin.read()
 
-    arch = "sm_75"
     tmp_result = code_line_reg.findall(sass_content)
     if not tmp_result:
         logging.error("Not found pairs")
         return
     # @todo: check is there any irregular code
-    for j, line in enumerate(tmp_result[:6]):
-        logging.info("\n%s" % line[0])
+    for j, line in enumerate(tmp_result):
+        logging.info("\nraw line:\t\t%s" % line[0])
         origin_inst = Inst(line)
         base = origin_inst.enc
+        bits = 0x0
+        positions = []
         # In volta and turing, len of the instruction code is 128bit
         for i in range(0, 128):
             # i from right to left
             mask = 2 ** i
-            bits = 0x0
-            pos = []
             newcode = base ^ mask
             dump_file_content = dump("0x{:032x}".format(newcode), arch, j)
             # print(dump_file_content)
@@ -49,9 +68,13 @@ def work():
                     logging.info("Opcode changes: %s => %s when bit [%d] is flipped from [%d]\t\tChange to:%s",
                                  origin_inst.op, tmp_inst.op, i, (base >> i) & 0x1, tmp_line[0])
                     bits = bits | (((base >> i) & 0x1) << i)
-                    pos.append(i)
-            # if len(pos) > 0:
-            # logging.info("0b{:0128b}".format(bits) + ": %s opcode bits %s: ", origin_inst.op, pos)
+                    positions.append(i)
+        # if len(positions) > 0:
+        #     logging.info("0b{:0128b}".format(bits) + ": %s opcode bits %s: ", origin_inst.op, positions)
+        if len(positions) > 0:
+            ops[origin_inst.op] = list(set(ops.get(origin_inst.op, []) + positions))
+    for node in ops:
+        logging.info("%s:\t[%s]", node, ",".join(str(x) for x in ops[node]))
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
 # NV Bin Explorer
 
+It's simple tool to explore CUDA binary file in CC7.0+.
+
+The input file is a cubin file where you can get it by `-keep` params when compile your CUDA C program.
+
+`com_lib.kernel_section_start_offset` is the kernel section entrance. Use `readelf -a ./test.cubin` to get your cubin file's kernel section start offset.
+
+execute:
+
+```bash
+python3 ./opcoder.py
+```
