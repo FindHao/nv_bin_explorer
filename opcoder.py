da366474e9d9e9bdf844954fc06192b6baa2f3c2
FindHao <find@findhao.net>
2019-04-26 14:56:19 +0800

    add argparser

--- opcoder.py
+++ opcoder.py
@@ -6,6 +6,7 @@ import re
 from dumper import dump
 from inst import Inst
 import com_lib
+import argparse
 
 sass_content = ''
 code_line_reg = re.compile(r'/\*.{4}\*/ +(.+?); +?/\* 0x(.+) \*/\n.+?/\* 0x(.{16}) \*/')
@@ -30,27 +31,35 @@ def init_sass_cubin_files(input_file, arch):
         logging.error("Error: when copy input cubin file:\t%s" % tmp_read)
 
 
-def work():
+def filter_change():
+    """Compare the origin inst and redisassember inst, and record the changed part"""
+    pass
+
+
+def work(input_file_name, output_file, section_start):
     global sass_content, ops
 
     arch = "sm_75"
-    # init_sass_cubin_files('test.cubin', arch)
-    logging.basicConfig(level=logging.INFO)
-    with open("test.sass", 'r')as fin:
-        sass_content = fin.read()
+    # for debug
+    # input_file_name = 'gaussian.cubin'
+    init_sass_cubin_files(input_file_name, arch)
+    logging.basicConfig(filename="./log/%s" % output_file, filemode="a", level=logging.INFO)
+    com_lib.kernel_section_start_offset = int(section_start, 16)
 
+    with open("%s.sass" % os.path.splitext(input_file_name)[0], 'r')as fin:
+        sass_content = fin.read()
     tmp_result = code_line_reg.findall(sass_content)
     if not tmp_result:
         logging.error("Not found pairs")
         return
     # @todo: check is there any irregular code
-    for j, line in enumerate(tmp_result):
+    for j, line in enumerate(tmp_result[:2800]):
         logging.info("\nraw line:\t\t%s" % line[0])
         origin_inst = Inst(line)
         base = origin_inst.enc
         bits = 0x0
         positions = []
-        # In volta and turing, len of the instruction code is 128bit
+        # In volta and turing, len of the instruction code is 128bitk
         for i in range(0, 128):
             # i from right to left
             mask = 2 ** i
@@ -73,10 +82,20 @@ def work():
         #     logging.info("0b{:0128b}".format(bits) + ": %s opcode bits %s: ", origin_inst.op, positions)
         if len(positions) > 0:
             ops[origin_inst.op] = list(set(ops.get(origin_inst.op, []) + positions))
+
     for node in ops:
         logging.info("%s:\t[%s]", node, ",".join(str(x) for x in ops[node]))
 
 
 if __name__ == "__main__":
-    pass
-    work()
+    parser = argparse.ArgumentParser(description='NVIDIA CUDA binary decoding for CC 7.0+ ')
+    parser.add_argument('-i', '--input', metavar='Input cubin file', required=True, dest='input_file_name',
+                        action='store')
+    # the script will not write to log file unless you define the output log file path
+    parser.add_argument('-o', '--output', metavar='decoding result', required=True, dest='output_path',
+                        action='store')
+    parser.add_argument('--section-start', metavar='Hex foramt. You can get it from readelf', required=True,
+                        dest='section_start',
+                        action='store')
+    args = parser.parse_args()
+    work(args.input_file_name, args.output_path, args.section_start)
